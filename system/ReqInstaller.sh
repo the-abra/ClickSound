c04ee58b028500468137b0bab8e3ac85a6afe417
Ege KILIÇ <aegean.sword1@gmail.com>
2023-07-04 23:48:30 +0300

    :wrench: Added req installer :wrench:

--- system/ReqInstaller.sh
+++ system/ReqInstaller.sh
@@ -1,3 +1,14 @@
+function apti() {
+    while ! [[ "$(whereis $1)" =~ "/" ]]; do
+    apti $1 -yq
+    done
+}
+
+apti bc
+apti wget
+apti curl
+apti python3
+apti pip
+apti sudo 
 pip install pynput
 pip install playsound
-
