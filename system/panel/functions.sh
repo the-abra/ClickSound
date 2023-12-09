f51f8cfab2ff2b2474b7217fee8a723cc08133b8
Ege KILIÇ <aegean.sword1@gmail.com>
2023-12-09 15:11:31 +0300

    Version βeta

--- system/panel/functions.sh
+++ system/panel/functions.sh
@@ -1,25 +1,25 @@
 echo -ne "${tp}"
 function LifeTime() {
-  target_date="2024-07-04"
-  today=$(date +%Y-%m-%d)
-  seconds_per_day=86400
+target_date="2024-07-04"
+today=$(date +%Y-%m-%d)
+seconds_per_day=86400
 
-  target_epoch=$(date -d "$target_date" +%s)
-  current_epoch=$(date -d "$today" +%s)
+target_epoch=$(date -d "$target_date" +%s)
+current_epoch=$(date -d "$today" +%s)
 
-  time_diff=$((target_epoch - current_epoch))
-  days_diff=$((time_diff / seconds_per_day))
+time_diff=$((target_epoch - current_epoch))
+days_diff=$((time_diff / seconds_per_day))
 
-  current_year=$(date -d "$today" +%Y)
-  leap_year=0
+current_year=$(date -d "$today" +%Y)
+leap_year=0
 
-  if (( (current_year % 4 == 0 && current_year % 100 != 0) || current_year % 400 == 0 )); then
-    leap_year=1
-  fi
+if (( (current_year % 4 == 0 && current_year % 100 != 0) || current_year % 400 == 0 )); then
+  leap_year=1
+fi
 
-  if (( leap_year == 1 && $(date -d "$today" +%m-%d) > "02-28" )); then
-    days_diff=$((days_diff + 1))
-  fi
+if [[ "$leap_year" == 1 && $(date -d "$today" +%m%d) > "0228" ]]; then
+  days_diff=$((days_diff + 1))
+fi
 }
 
 function getstatus() {
